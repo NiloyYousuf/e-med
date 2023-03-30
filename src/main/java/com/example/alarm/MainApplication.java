106b96108382ed3df75ab1dce99633d38ac2b2c6
Linda Clark <ramisaudhi125@gmail.com>
2023-03-30 16:40:10 +0600

    Audhi

--- src/main/java/com/example/alarm/MainApplication.java
+++ src/main/java/com/example/alarm/MainApplication.java
@@ -9,11 +9,12 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("searchpage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("loginpage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 720, 480);
         stage.setTitle("e-MED");
         stage.setScene(scene);
         stage.show();
+        System.out.println("HiNoliAmiAudhi");
     }
 
     public static void main(String[] args) {
